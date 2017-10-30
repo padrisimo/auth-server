0d82b0816a9bc34799cbf4e5470bfc5439fe7925
adrian.fernandez.externo@axa-assistance.es <adrian.fernandez.externo@axa-assistance.es>
2017-10-30 17:12:31 +0100

    express route handler

---
+++ models/user.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Define our model
+const userSchema = new Schema({
+    
+});
+
+// Create the model class
+
+// Export the model
