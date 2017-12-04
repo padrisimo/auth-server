4df08a35c417975e8a48c9e73318076893613329
adrian.fernandez.externo@axa-assistance.es <adrian.fernandez.externo@axa-assistance.es>
2017-12-04 17:23:59 +0100

    creating user records

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,16 +1,29 @@
 const User = require('../models/user');
 
-exports.signup = function(req, res, next) {
+exports.signup = function (req, res, next) {
     const email = req.body.email;
     const password = req.body.password;
-    
+
     // see if a user with the given email exits
-    User.findOne({ email: email }, function(err, existingUser){
-        //
+    User.findOne({ email: email }, function (err, existingUser) {
+        if (err) { return next(err); }
+
+        // If a user with same email already exist, return an error
+        if (existingUser) {
+            return res.status(422).send({ error: 'Email is in use' })
+        }
+
+        // if a user with email don't exits, crate and save user record
+        const user = new User({
+            email: email,
+            password: password
+        })
+
+        user.save(function (err) {
+            if (err) { return next(err); }
+            
+            // Respond to request indicating the user was created
+            res.json({ success: true });
+        });
     });
-    // If a user with same email already exist, return an error
-
-    // if a user with email don't exits, crate and save user record
-
-    // Respond to request indicating the user was created
 }
