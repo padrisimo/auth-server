2abbd7152271cc2c1b2666e5382842b9790b72a1
adrian.fernandez.externo@axa-assistance.es <adrian.fernandez.externo@axa-assistance.es>
2017-12-04 16:38:21 +0100

    searching for Users

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,3 +1,16 @@
+const User = require('../models/user');
+
 exports.signup = function(req, res, next) {
-    res.send({succes: 'true'});
+    const email = req.body.email;
+    const password = req.body.password;
+    
+    // see if a user with the given email exits
+    User.findOne({ email: email }, function(err, existingUser){
+        //
+    });
+    // If a user with same email already exist, return an error
+
+    // if a user with email don't exits, crate and save user record
+
+    // Respond to request indicating the user was created
 }
