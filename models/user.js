dc658b2a6398a28c2cd624376642b1a564e4ac83
adrian.fernandez.externo@axa-assistance.es <adrian.fernandez.externo@axa-assistance.es>
2017-12-05 12:52:20 +0100

    commets tips on salting passwords

--- models/user.js
+++ models/user.js
@@ -9,15 +9,20 @@ const userSchema = new Schema({
 });
 
 // On save Hook, encrypt password
+// Before saving model, run this function
 userSchema.pre('save', function (next) {
+    //get access to user model 
     const user = this;
 
+    //generate a salt
     bcrypt.genSalt(10, function (err, salt) {
         if (err) { return next(err); }
 
+        // hash (encypt) my password using the salt
         bcrypt.hash(user.password, salt, null, function (err, hash) {
             if (err) { return next(err); }
 
+            //overwrite plain text password with encrypted password
             user.password = hash;
             next();
         })
