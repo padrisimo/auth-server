4b66c44e99b50aa3a76f4687ecf3d2a375c3d557
adrian.fernandez.externo@axa-assistance.es <adrian.fernandez.externo@axa-assistance.es>
2017-12-13 14:40:16 +0100

    passport strategies

--- services/passport.js
+++ services/passport.js
@@ -3,3 +3,22 @@ const User = require('../models/user');
 const config = require('../config');
 const JwtStrategy = require('passport-jwr').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+
+// setup options for jwt strategy
+const jwtOptions = {};
+
+// jwt strategy
+const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
+    // user.id ? done : !user
+    User.findById(payload.sub, function (err, user) {
+        if (err) { return done(err, false); }
+
+        if (user) {
+            done(null, user);
+        } else {
+            done(null, false);
+        }
+    })
+});
+
+// tell passport to use this strategy
