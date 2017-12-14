4d2e6e0061255181ed37c12c3dc22b401e81d7c9
adrian.fernandez.externo@axa-assistance.es <adrian.fernandez.externo@axa-assistance.es>
2017-12-14 16:55:08 +0100

    signin users

--- services/passport.js
+++ services/passport.js
@@ -6,7 +6,7 @@ const ExtractJwt = require('passport-jwt').ExtractJwt;
 const LocalStrategy = require('passport-local');
 
 // Create local strategy
-const localOptions = { username: 'email' };
+const localOptions = { usernameField  : 'email' };
 const localLogin = new LocalStrategy(localOptions, function (email, password, done) {
     User.findOne({ email: email }, function (err, user) {
         if (err) { return done(err); }
